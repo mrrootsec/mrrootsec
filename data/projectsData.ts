f10c9f183bdc63df8833c9927f6cfe556daf0522
MOHAMMAD SAQLAIN <32585833+mrrootsec@users.noreply.github.com>
2026-03-18 10:14:36 +0530

    Add ReTab project to projectsData

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -13,6 +13,12 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/assetviz.png',
     href: 'https://github.com/mrrootsec/AssetViz',
   },
+  {
+    title: 'ReTab',
+    description: ` Anyone who has used Burp Suite Repeater for more than 20 minutes knows the problem. You send request after request, and every tab is named 1, 2, 3... Ten tabs in, you are clicking through each one trying to find that one password reset endpoint you tested earlier. Fifty tabs in, you have lost track entirely.`,
+    imgSrc: '/static/images/retab.png',
+    href: 'https://github.com/mrrootsec/ReTab',
+  }
 ]
 
 export default projectsData
